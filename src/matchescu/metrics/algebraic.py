540ae4bb1c33bc014d57d5979938bef8c2fb1a82
Andrei Olar <andrei.olar@dpgmedia.nl>
2023-11-10 13:36:46 +0200

    fix: reimplement rand, adj_rand, twi

--- src/matchescu/metrics/algebraic.py
+++ src/matchescu/metrics/algebraic.py
@@ -1,106 +1,125 @@
 import itertools
-from math import sqrt
+from functools import reduce
 from typing import Iterable, Generator
 
-import numpy.dtypes
-from numpy import ndarray, zeros, sum, vectorize, dtype
+import numpy as np
 
 
-def extract_algebraic_result_model(input_data: Iterable[Iterable[Iterable]]) -> list[set[tuple]]:
+def extract_algebraic_result_model(
+    input_data: Iterable[Iterable[Iterable]],
+) -> list[set[tuple]]:
     return [
-        set(
-            tuple(v for v in partition_item)
-            for partition_item in partition_class
-        )
+        set(tuple(v for v in partition_item) for partition_item in partition_class)
         for partition_class in input_data
     ]
 
 
-def _compute_partition_overlap(
-        ground_truth: list[set[tuple]], result: list[set[tuple]]
-) -> list[set[tuple]]:
-    retval = []
-    for i in ground_truth:
-        for j in result:
-            overlap = i & j
-            if len(overlap) < 1:
-                continue
-            retval.append(overlap)
-    return retval
-
-
-def _compute_intersection_matrix(
-        ground_truth: list[set[tuple]], result: list[set[tuple]]
-) -> ndarray:
-    retval = zeros((len(ground_truth), len(result)), dtype=int)
-    for i, class_i in enumerate(ground_truth):
-        for j, class_j in enumerate(result):
-            retval[i, j] = len(class_i & class_j)
-    return retval
-
-
-def _compute_cn2(n: int) -> int:
-    return (n * (n - 1)) // 2
-
-
-def _compute_total_number_of_elements(ground_truth: list[set[tuple]]) -> int:
-    reconstructed = set()
-    for partition_class in ground_truth:
-        reconstructed |= partition_class
-    return len(reconstructed)
-
-
 def twi(ground_truth: list[set[tuple]], result: list[set[tuple]]) -> float:
-    partition_overlap = _compute_partition_overlap(ground_truth, result)
-    if len(partition_overlap) == 0:
-        return 0
-    return sqrt(len(ground_truth) * len(result)) / len(partition_overlap)
+    """
+    Compute the Talburt-Wang index for entity resolution.
+
+    Parameters:
+    ground_truth (List[Set]): The ground truth against which we compare the entity resolution results.
+    resolution_results (List[Set]): The entity resolution results.
+
+    Returns:
+    float: The Talburt-Wang index.
+    """
+    # Initialize the numerator and denominator of the Talburt-Wang index
+    numerator = 0
+    denominator = 0
+
+    # Iterate over each set in the ground truth
+    for gt_set in ground_truth:
+        # Find the corresponding set in the resolution results
+        for res_set in result:
+            # Compute the intersection of the two sets
+            intersection = gt_set & res_set
+
+            # Update the numerator and denominator
+            numerator += len(intersection) ** 2
+            denominator += len(gt_set) * len(res_set)
+
+    # Compute the Talburt-Wang index
+    tw_index = numerator / denominator if denominator != 0 else 0
+
+    return tw_index
 
 
-def _compute_rand_index_components(
-        ground_truth: list[set[tuple]], result: list[set[tuple]]
-) -> tuple[int, int, int, int]:
-    intersection_matrix = _compute_intersection_matrix(ground_truth, result)
-    si = list(map(len, ground_truth))
-    sj = list(map(len, result))
-    smn = _compute_total_number_of_elements(ground_truth)
-    try:
-        cn2 = vectorize(_compute_cn2, otypes=numpy.double)
-
-        x = sum(cn2(intersection_matrix))
-        y = sum(cn2(si))
-        z = sum(cn2(sj))
-
-        return x, y, z, cn2(smn) - x - y - z
-    except ValueError:
-        return 0, 0, 0, 0
+def _get_unique_identifiers(partition: list[set[tuple]]) -> list[int]:
+    result = {}
+    for cluster in partition:
+        key = frozenset(cluster)
+        result[key] = hash(key)
+    return list(result.values())
 
 
-def rand_index(ground_truth: list[set[tuple]], result: list[set[tuple]]) -> float:
-    x, y, z, w = _compute_rand_index_components(ground_truth, result)
-    if (x + y + z + w) == 0:
-        return 0
-    return (x + w) / (x + y + z + w)
+def _pairs(cluster: set[tuple]) -> Generator[tuple, None, None]:
+    yield from itertools.combinations(cluster, 2)
 
 
-def adjusted_rand_index(ground_truth: list[set[tuple]], result: list[set[tuple]]) -> float:
-    x, y, z, w = _compute_rand_index_components(ground_truth, result)
-    if (x + y + z + w) == 0:
-        return 0
-    qty = ((y + x) * (z + x)) / (x + y + z + w)
+def rand_index(ground_truth: list[set], resolution: list[set]) -> float:
+    a, b, c, d = 0, 0, 0, 0
 
-    return (x - qty) / (((y + z + 2 * x) / 2) - qty)
+    for gt_cluster in ground_truth:
+        for er_cluster in resolution:
+            gt_cluster_pairs = set(_pairs(gt_cluster))
+            er_cluster_pairs = set(_pairs(er_cluster))
+            same_pair_count = len(gt_cluster_pairs & er_cluster_pairs)
+            only_gt_pair_count = len(gt_cluster_pairs - er_cluster_pairs)
+            only_er_pair_count = len(er_cluster_pairs - gt_cluster_pairs)
+            a += same_pair_count
+            b += only_gt_pair_count
+            c += only_er_pair_count
+            d += only_gt_pair_count + only_er_pair_count
+
+    return (a + d) / (a + b + c + d)
 
 
-def _pairs(input_data: list[set[tuple]]) -> Generator[tuple[tuple], None, None]:
+def _comb_n_2(value: int) -> int:
+    return (value * (value - 1)) // 2
+
+
+def adjusted_rand_index(
+    ground_truth: list[set[tuple]], result: list[set[tuple]]
+) -> float:
+    initial_data_size = reduce(
+        lambda count, cluster: count + len(cluster), ground_truth, 0
+    )
+    cn2 = _comb_n_2(initial_data_size)
+
+    contingency_table = np.array(
+        [
+            [len(gt_cluster & er_cluster) for er_cluster in result]
+            for gt_cluster in ground_truth
+        ],
+        dtype=np.int32,
+    )
+    a = np.sum(contingency_table, axis=1)
+    b = np.sum(contingency_table, axis=0)
+    comb_2 = np.vectorize(_comb_n_2)
+    x = np.sum(comb_2(contingency_table))
+    y = np.sum(comb_2(a))
+    w = np.sum(comb_2(b))
+    z = (y * w) / cn2
+    if y + w == 2 * z:
+        return 1.0
+    ari = 2 * (x - z) / ((y + w) - 2 * z)
+
+    return ari
+
+
+def _input_data_pairs(
+    input_data: list[set[tuple]],
+) -> Generator[tuple[tuple], None, None]:
     for partition_class in input_data:
         for pair in itertools.combinations(partition_class, 2):
             yield pair
 
 
 def pair_precision(ground_truth: list[set[tuple]], result: list[set[tuple]]) -> float:
-    gt_pairs = set(_pairs(ground_truth))
-    res_pairs = set(_pairs(result))
+    gt_pairs = set(_input_data_pairs(ground_truth))
+    res_pairs = set(_input_data_pairs(result))
     if len(res_pairs) == 0:
         # no pairs were retrieved -> precision = 0
         return 0.0
@@ -108,12 +127,14 @@ def pair_precision(ground_truth: list[set[tuple]], result: list[set[tuple]]) -> 
 
 
 def pair_recall(ground_truth: list[set[tuple]], result: list[set[tuple]]) -> float:
-    gt_pairs = set(_pairs(ground_truth))
-    res_pairs = set(_pairs(result))
+    gt_pairs = set(_input_data_pairs(ground_truth))
+    res_pairs = set(_input_data_pairs(result))
     return len(gt_pairs & res_pairs) / (len(gt_pairs))
 
 
-def pair_comparison_measure(ground_truth: list[set[tuple]], result: list[set[tuple]]) -> float:
+def pair_comparison_measure(
+    ground_truth: list[set[tuple]], result: list[set[tuple]]
+) -> float:
     pp = pair_precision(ground_truth, result)
     pr = pair_recall(ground_truth, result)
     if pp + pr == 0:
@@ -123,12 +144,12 @@ def pair_comparison_measure(ground_truth: list[set[tuple]], result: list[set[tup
 
 
 def _cluster(input_data: list[set[tuple]]) -> Generator[tuple[tuple], None, None]:
-    return (
-        tuple(v for v in partition_class) for partition_class in input_data
-    )
+    return (tuple(v for v in partition_class) for partition_class in input_data)
 
 
-def cluster_precision(ground_truth: list[set[tuple]], result: list[set[tuple]]) -> float:
+def cluster_precision(
+    ground_truth: list[set[tuple]], result: list[set[tuple]]
+) -> float:
     gt_cluster = set(_cluster(ground_truth))
     res_cluster = set(_cluster(result))
     if len(res_cluster) == 0:
@@ -145,7 +166,9 @@ def cluster_recall(ground_truth: list[set[tuple]], result: list[set[tuple]]) -> 
     return len(gt_cluster & res_cluster) / len(gt_cluster)
 
 
-def cluster_comparison_measure(ground_truth: list[set[tuple]], result: list[set[tuple]]) -> float:
+def cluster_comparison_measure(
+    ground_truth: list[set[tuple]], result: list[set[tuple]]
+) -> float:
     cp = cluster_precision(ground_truth, result)
     cr = cluster_recall(ground_truth, result)
     if cp + cr == 0:
